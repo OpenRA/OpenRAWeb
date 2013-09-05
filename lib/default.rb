9cecfc8bad15c0085c31a07d0975e25087698fc0
Paul Chote <paul@chote.net>
2013-09-05 20:54:46 +1200

    New download page.

--- lib/default.rb
+++ lib/default.rb
@@ -23,56 +23,5 @@ module OpenRAHelpers
         #doc = Nokogiri::HTML(post.compiled_content)
         #doc.xpath("//text()").to_s
     end
-
-    def download_helper(platform)
-        ret = ""
-        RELEASE_TYPES.each do |release_type|
-            url = url_for_download(platform, release_type)
-            size = size_of_download(platform, release_type)
-            ret += <<-HTML
-                <div id="DownloadLatest" style="clear: both;">
-                    <a href="#{url}"><img src="/images/platforms/#{platform}.png" alt="#{platform}.png" /></a>
-                    <div style="padding-top: 15px;">
-                        <a href="#{url}" title="Download #{release_type.capitalize} Version">Download #{release_type.capitalize} Version</a><br />
-                        (#{size})
-                    </div>
-                </div>
-            HTML
-        end
-        ret
-    end
-
-    def package_list(platform, release_type)
-      case platform
-          when "osx"
-              pattern = "OpenRA-*.zip"
-          when "win"
-              pattern = "OpenRA-*.exe"
-          when "linux/arch"
-              pattern = "openra-*-1-any.pkg.tar.xz"
-          when "linux/deb"
-              pattern = "openra_*_all.deb"
-          when "linux/rpm"
-              pattern = "openra-*-1.noarch.rpm"
-          else
-              raise "Why is your platform #{platform}?!?!"
-      end
-      Dir.glob("#{PACKAGE_DIR}/#{platform}/#{release_type}/#{pattern}")
-    end
-
-    def url_for_download(platform, release_type)
-        package = File.basename(package_list(platform, release_type).last || "")
-        "#{release_type}/#{package}"
-    end
-
-    def size_of_download(platform, release_type)
-        package = package_list(platform, release_type).last
-        size = if package then (File.stat(package).size / 1048576.0).to_s[0..3] else '???' end
-        "#{size} MB"
-    end
-
-    def name_of_download(platform, release_type)
-        File.basename(package_list(platform, release_type).last || "")
-    end
 end
 include OpenRAHelpers
