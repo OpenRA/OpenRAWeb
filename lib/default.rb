16e7c0f31fb7b351e40679880a556c6c94f8f2a4
Matthias Mailänder <matthias@mailaender.name>
2013-12-14 20:07:49 +0100

    refactor
    * don't duplicate playtest/release in local variables
    * removed unused variables
    * put all OpenRA helpers in one file

--- lib/default.rb
+++ lib/default.rb
@@ -5,24 +5,14 @@ include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::XMLSitemap
 
-module OpenRAHelpers
-    RELEASE_TYPES = ["release", "playtest"]
-    PACKAGE_DIR = '/path/to/packages/dir/'
+def post_snippet(post)
+    # For 'real' snippets, html tags should be stripped before text processing. 
+    # But really, it's a static site - we don't need snippets :)
+    post.compiled_content
 
-    def pretty_date(date)
-        attribute_to_time(date).strftime("%Y-%m-%d")
-    end
-
-    def post_snippet(post)
-        # For 'real' snippets, html tags should be stripped before text processing. 
-        # But really, it's a static site - we don't need snippets :)
-        post.compiled_content
-
-        # This will strip ALL tags, we want to whitelist *some*
-        # Yes, this is a heavyweight dependency, but it's only used at compile time
-        #require 'nokogiri'
-        #doc = Nokogiri::HTML(post.compiled_content)
-        #doc.xpath("//text()").to_s
-    end
+    # This will strip ALL tags, we want to whitelist *some*
+    # Yes, this is a heavyweight dependency, but it's only used at compile time
+    #require 'nokogiri'
+    #doc = Nokogiri::HTML(post.compiled_content)
+    #doc.xpath("//text()").to_s
 end
-include OpenRAHelpers
