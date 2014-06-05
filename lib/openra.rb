fac51a86d240fb2f69b10f567fae082204711d53
Matthias Mailänder <matthias@mailaender.name>
2014-06-05 17:33:35 +0200

    now in MarkDown and directly link it

--- lib/openra.rb
+++ lib/openra.rb
@@ -50,7 +50,7 @@ PLATFORM_BLURB = {
     You can get unstable playtests using the following overlay:<br />
     <pre>http://github.com/cerebrum/dr/raw/master/repo.xml</pre><br />",
     "arch" => "The stable version is also available in the <a href=\"https://www.archlinux.org/packages/community/any/openra/\">official Arch Linux repositories</a>.",
-    "source" => "Follow the instructions in the INSTALL document to build and run OpenRA.<br />
+    "source" => "Follow the instructions in the <a href=\"https://github.com/OpenRA/OpenRA/blob/bleed/INSTALL.md\">INSTALL.md</a> document to build and run OpenRA.<br />
     <a title=\"Visual C# Express Download\" href=\"http://www.microsoft.com/express/downloads/\">Visual C# Express</a> (Windows) and <a title=\"MonoDevelop\" href=\"http://www.monodevelop.com/\"/>MonoDevelop</a> (OS X / Linux) are free IDEs that work with OpenRA.<br /><br />
     If you'd like to <a href=\"https://github.com/OpenRA/OpenRA/pulls\">contribute patches</a> (or just don't want to fiddle with tar files) you can download and/or update the code using the <a href=\"http://git-scm.com/\">git version control system</a>:<br />
     <pre>$ git clone git://github.com/OpenRA/OpenRA.git</pre><br />",
