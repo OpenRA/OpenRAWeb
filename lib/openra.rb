16068eddab658a89b09df9274a04b4829632071a
Paul Chote <paul@chote.net>
2014-06-08 15:11:43 +1200

    Overhaul download page generation.
    - Remove remaining ties to the res0l server.
    - Use GH API instead of raw http.
    - Remove unnecessary duplication.

--- lib/openra.rb
+++ lib/openra.rb
@@ -1,9 +1,11 @@
-DOWNLOAD_BASE_PATH = "http://openra.res0l.net/assets/downloads/"
-DOWNLOAD_GITHUB_RELEASE_PATH = "https://github.com/OpenRA/OpenRA/releases/download/"
-DOWNLOAD_GITHUB_SOURCE_PATH = "https://github.com/OpenRA/OpenRA/archive/"
+DOWNLOAD_GITHUB_BASE_PATH = "https://github.com/OpenRA/OpenRA/"
 
-PLAYTEST_TAG = "playtest-20140602"
-RELEASE_TAG = "release-20131223"
+# Disable if you're doing something that may hit the gh rate limit (60 queries per hour for non-authenticated users)
+ENABLE_GITHUB_API = true
+
+# Github release IDs: obtain from https://api.github.com/repos/OpenRA/OpenRA/releases
+GITHUB_PLAYTEST_ID = '362233'
+GITHUB_RELEASE_ID = '176704'
 
 PAGES = {
 	"/" => "Home",
@@ -13,18 +15,42 @@ PAGES = {
 	"/community/" => "Community"
 }
 
-# TODO: Remove this gross duplication
-PLATFORMS = ["win", "osx", "deb", "rpm", "arch", "gentoo", "source", "desura"]
+PLATFORMS = {
+    "win" => {
+      name: "Windows",
+      desc: "The default GPU drivers included with Windows do not support OpenGL rendering.<br />You may need to install full drivers supplied by your GPU vendor."
+    },
+    "osx" => {
+      name: "OS X",
+      desc: "OpenRA requires Mono 2.10 or greater (3.2 or greater recommended).<br /><a href=\"http://www.go-mono.com/mono-downloads/download.html\">Download Mono</a>."
+    }, 
+    "deb" => {
+      name: "Debian / Ubuntu",
+      desc: "The stable version is also available from <a href=\"http://www.playdeb.net/software/OpenRA\">PlayDeb</a>."
+    },
 
-PLATFORM_NAME = {
-    "win" => "Windows",
-    "osx" => "OS X",
-    "deb" => "Debian / Ubuntu",
-    "rpm" => "Fedora / openSUSE",
-    "arch" => "Arch Linux",
-    "gentoo" => "Gentoo",
-    "source" => "Source Code",
-    "desura" => "Desura"
+    "rpm" => {
+      name: "Fedora / openSUSE",
+      desc: "The stable version is also available in the <a href=\"http://software.opensuse.org/download.html?project=games&package=openra\">official openSUSE games repository.</a>"
+    },
+
+    "arch" => {
+      name: "Arch Linux",
+      desc: "The stable version is also available in the <a href=\"https://www.archlinux.org/packages/community/any/openra/\">official Arch Linux repositories</a>."
+    },
+
+    "gentoo" => {
+      name: "Gentoo",
+      desc: "Stable versions are packaged in the <a href=\"http://packages.gentoo.org/package/games-strategy/openra\">official Gentoo repositories</a>.<br /><br />To install the ebuild:<br /><pre>$ emerge -av openra</pre><br />You can get unstable playtests using the following overlay:<br /><pre>http://github.com/cerebrum/dr/raw/master/repo.xml</pre><br />"
+    },
+    "source" => {
+      name: "Source Code",
+      desc: "Follow the instructions in the <a href=\"https://github.com/OpenRA/OpenRA/blob/bleed/INSTALL.md\">INSTALL.md</a> document to build and run OpenRA.<br /><a title=\"Visual C# Express Download\" href=\"http://www.microsoft.com/express/downloads/\">Visual C# Express</a> (Windows) and <a title=\"MonoDevelop\" href=\"http://www.monodevelop.com/\"/>MonoDevelop</a> (OS X / Linux) are free IDEs that work with OpenRA.<br /><br />If you'd like to <a href=\"https://github.com/OpenRA/OpenRA/pulls\">contribute patches</a> (or just don't want to fiddle with tar files) you can download and/or update the code using the <a href=\"http://git-scm.com/\">git version control system</a>:<br /><pre>$ git clone git://github.com/OpenRA/OpenRA.git</pre><br />"
+    },
+    "desura" => {
+      name: "Desura",
+      desc: "Desura is a community driven digital distribution service for gamers, putting the best games, mods and downloadable content from developers at gamers fingertips."
+    }
 }
 
 SOCIAL = {
@@ -36,24 +62,6 @@ SOCIAL = {
   "github" => { name: "Github", url: "https://github.com/OpenRA/OpenRA"}
 }
 
-PLATFORM_BLURB = {
-    "win" => "The default GPU drivers included with Windows do not support OpenGL rendering.<br />You may need to install full drivers supplied by your GPU vendor.",
-    "osx" => "OpenRA requires Mono 2.10 or greater (3.2 or greater recommended).<br /><a href=\"http://www.go-mono.com/mono-downloads/download.html\">Download Mono</a>.",
-    "deb" => "The stable version is also available from <a href=\"http://www.playdeb.net/software/OpenRA\">PlayDeb</a>.",
-    "rpm" => "The stable version is also available in the <a href=\"http://software.opensuse.org/download.html?project=games&package=openra\">official openSUSE games repository.</a>",
-    "gentoo" => "Stable versions are packaged in the <a href=\"http://packages.gentoo.org/package/games-strategy/openra\">official Gentoo repositories</a>.<br /><br />
-    To install the ebuild:<br />
-    <pre>$ emerge -av openra</pre><br />
-    You can get unstable playtests using the following overlay:<br />
-    <pre>http://github.com/cerebrum/dr/raw/master/repo.xml</pre><br />",
-    "arch" => "The stable version is also available in the <a href=\"https://www.archlinux.org/packages/community/any/openra/\">official Arch Linux repositories</a>.",
-    "source" => "Follow the instructions in the <a href=\"https://github.com/OpenRA/OpenRA/blob/bleed/INSTALL.md\">INSTALL.md</a> document to build and run OpenRA.<br />
-    <a title=\"Visual C# Express Download\" href=\"http://www.microsoft.com/express/downloads/\">Visual C# Express</a> (Windows) and <a title=\"MonoDevelop\" href=\"http://www.monodevelop.com/\"/>MonoDevelop</a> (OS X / Linux) are free IDEs that work with OpenRA.<br /><br />
-    If you'd like to <a href=\"https://github.com/OpenRA/OpenRA/pulls\">contribute patches</a> (or just don't want to fiddle with tar files) you can download and/or update the code using the <a href=\"http://git-scm.com/\">git version control system</a>:<br />
-    <pre>$ git clone git://github.com/OpenRA/OpenRA.git</pre><br />",
-    "desura" => "Desura is a community driven digital distribution service for gamers, putting the best games, mods and downloadable content from developers at gamers fingertips.",
-}
-
 def package_name(platform, tag)
     modtag = tag.gsub('-', '.')
     case platform
@@ -62,7 +70,7 @@ def package_name(platform, tag)
         when "win"
             "OpenRA-#{tag}.exe"
         when "arch"
-            "openra-#{modtag}-1-any.pkg.tar.xz"
+            "openra-#{modtag}-1-any.pkg.tar.gz"
         when "deb"
             "openra_#{modtag}_all.deb"
         when "rpm"
@@ -74,51 +82,43 @@ def package_name(platform, tag)
     end
 end
 
-def package_url(platform, tag)
-    DOWNLOAD_BASE_PATH + package_path(platform) + package_name(platform, tag)
+def generate_download_button(platform, github_id, tag, sizes)
+  if github_id == "" then
+    "<span>No playtest available<br />(release is newer)</span>"
+  elsif platform == "source"
+    url = DOWNLOAD_GITHUB_BASE_PATH + "archive/#{tag}.tar.gz"
+    sprintf('<a href="%s" title=\"Download %s">Download %s<br />(source package)</a>', url, tag, tag)
+  else
+    package = package_name(platform, tag)
+    url = DOWNLOAD_GITHUB_BASE_PATH + "releases/download/" + tag + '/' + package
+    size = sizes.key?(package) ? sprintf("(%.2f MB)", sizes[package] / 1048576.0) : "(size unknown)"
+    sprintf('<a href="%s" title=\"Download %s">Download %s<br />%s</a>', url, tag, tag, size)
+  end
 end
 
-def mirrored_package_url(platform, tag)
-    case platform 
-        when "source"
-            DOWNLOAD_GITHUB_SOURCE_PATH + package_name(platform, tag)
-        else
-            DOWNLOAD_GITHUB_RELEASE_PATH + tag + '/' + package_name(platform, tag)
-    end
+def fetch_package_sizes(gh_release_ids)
+ require 'octokit'
+ s = Hash.new
+ if ENABLE_GITHUB_API then
+   gh_release_ids.each do |id|
+     if id == "" then next end
+     assets = Octokit.release_assets('https://api.github.com/repos/OpenRA/OpenRA/releases/' + id)
+     assets.each do |asset|
+       s[asset.name] = asset.size
+     end
+   end
+ end
+ s
 end
 
-def package_path(platform)
-    case platform
-        when "osx"
-            "mac/"
-        when "win"
-            "windows/"
-        when "arch"
-            "linux/arch/"
-        when "deb"
-            "linux/deb/"
-        when "rpm"
-            "linux/rpm/"
-        when "source"
-            "source/"
-        else
-            raise "Why is your platform #{platform}?!?!"
-    end
-end
-
-def package_size(platform, tag)
-    require 'net/http'
-    size = ""
-    begin
-        uri = URI.parse(package_url(platform, tag))
-        http = Net::HTTP.start(uri.host, uri.port)
-        http.request_head(uri.path) {|response|
-            size = sprintf("%.2f MB", Integer(response['content-length']) / 1048576.0)
-        }
-    rescue
-        size = "??? MB"
-    end
-    size
+def fetch_git_tag(github_id)
+  require 'octokit'
+  if ENABLE_GITHUB_API and github_id != '' then
+    asset = Octokit.release_asset('https://api.github.com/repos/OpenRA/OpenRA/releases/' + github_id)
+    asset.tag_name
+  else
+    "unknown-12345678"
+  end
 end
 
 def pretty_date(date)
