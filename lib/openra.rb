92051aeefb3610f4f14f05d1db21c383ee290f4d
Paul Chote <paul@chote.net>
2014-06-09 01:02:39 +1200

    Update release ID.

--- lib/openra.rb
+++ lib/openra.rb
@@ -5,7 +5,7 @@ ENABLE_GITHUB_API = true
 
 # Github release IDs: obtain from https://api.github.com/repos/OpenRA/OpenRA/releases
 GITHUB_PLAYTEST_ID = ''
-GITHUB_RELEASE_ID = '362233'
+GITHUB_RELEASE_ID = '363007'
 
 PAGES = {
 	"/" => "Home",
