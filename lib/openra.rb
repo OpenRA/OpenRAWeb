dea3c605d2d3b1dbe2e5bb1ae5c98ee0e8c5297a
Paul Chote <paul@chote.net>
2014-09-28 00:16:37 +1200

    Update playtest id.

--- lib/openra.rb
+++ lib/openra.rb
@@ -4,7 +4,7 @@ DOWNLOAD_GITHUB_BASE_PATH = "https://github.com/OpenRA/OpenRA/"
 ENABLE_GITHUB_API = true
 
 # Github release IDs: obtain from https://api.github.com/repos/OpenRA/OpenRA/releases
-GITHUB_PLAYTEST_ID = ''
+GITHUB_PLAYTEST_ID = '587108'
 GITHUB_RELEASE_ID = '443608'
 
 PAGES = {
