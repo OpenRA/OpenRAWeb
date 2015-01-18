441d1a19d948fa20a8f455979a5248a1637c98d2
Matthias Mailänder <Mailaender@users.noreply.github.com>
2015-01-18 09:27:46 +0100

    Update update-github-pages.sh
    set -e and move git config --global credential.helper down

--- update-github-pages.sh
+++ update-github-pages.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -e
 
-git config --global credential.helper "store --file=~/.git-credentials"
-echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 git config --global user.email "orabot@users.noreply.github.com"
 git config --global user.name "orabot"
+git config --global credential.helper "store --file=~/.git-credentials"
+echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
 cd "$HOME"
 git clone --branch=master https://${GH_TOKEN}@github.com/OpenRA/openra.github.io openra.net > /dev/null
-cd openra.net || exit 1
-cp -rf "$TRAVIS_BUILD_DIR"/output/* . || exit 1
+cd openra.net
+cp -rf "$TRAVIS_BUILD_DIR"/output/* .
 
 git add *
 git commit -m "Deploy OpenRAWeb HTML to GitHub pages
