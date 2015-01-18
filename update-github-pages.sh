59d960f029743e4da771d6ee6242193cfef9df94
Matthias Mailänder <matthias@mailaender.name>
2015-01-18 08:57:54 +0100

    go for global git config

--- update-github-pages.sh
+++ update-github-pages.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-git config credential.helper "store --file=.git/credentials"
-echo "https://$GH_TOKEN:@github.com" > .git/credentials
+git config --global credential.helper "store --file=~/.git-credentials"
+echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+git config --global user.email "orabot@users.noreply.github.com"
+git config --global user.name "orabot"
 
 cd "$HOME"
 git clone --branch=master https://github.com/OpenRA/openra.github.io openra.net || exit 1
 cd openra.net || exit 1
 cp -rf "$TRAVIS_BUILD_DIR"/output/* . || exit 1
-
-git config --local user.email "orabot@users.noreply.github.com"
-git config --local user.name "orabot"
 
 git add *
 git commit -m "Deploy OpenRAWeb HTML to GitHub pages
