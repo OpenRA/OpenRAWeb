ea9b30a70b71384d90564f3e133605f41969d855
Matthias Mailänder <Mailaender@users.noreply.github.com>
2015-01-18 09:31:34 +0100

    Update update-github-pages.sh
    missing colon maybe?

--- update-github-pages.sh
+++ update-github-pages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-git config --global user.email "orabot@users.noreply.github.com"
 git config --global user.name "orabot"
+git config --global user.email "orabot@users.noreply.github.com"
 git config --global credential.helper "store --file=~/.git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
 cd "$HOME"
-git clone --branch=master https://${GH_TOKEN}@github.com/OpenRA/openra.github.io openra.net > /dev/null
+git clone --branch=master https://$GH_TOKEN:@github.com/OpenRA/openra.github.io openra.net > /dev/null
 cd openra.net
 cp -rf "$TRAVIS_BUILD_DIR"/output/* .
 
