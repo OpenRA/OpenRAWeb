d830cc3390a52e2034888419a486aacb3b173920
Matthias Mailänder <Mailaender@users.noreply.github.com>
2015-01-18 09:41:17 +0100

    Update update-github-pages.sh
    use syntax from http://git-scm.com/book/en/v2/Git-Tools-Credential-Storage

--- update-github-pages.sh
+++ update-github-pages.sh
@@ -3,11 +3,11 @@ set -e
 
 git config --global user.name "orabot"
 git config --global user.email "orabot@users.noreply.github.com"
-git config --global credential.helper "store --file=~/.git-credentials"
-echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+git config --global credential.helper store --file ~/.openra-credentials
+echo "https://$GH_TOKEN:@github.com" > ~/.openra-credentials
 
 cd "$HOME"
-git clone --branch=master https://$GH_TOKEN:@github.com/OpenRA/openra.github.io openra.net > /dev/null
+git clone --branch=master https://${GH_TOKEN}@github.com/OpenRA/openra.github.io openra.net > /dev/null
 cd openra.net
 cp -rf "$TRAVIS_BUILD_DIR"/output/* .
 
