97b0b481dcacb89b78afb5629c548ac98f92f2f2
Oliver Brakmann <ob-web2013+github@posteo.net>
2015-12-13 12:50:45 +0100

    Add news post for playtest-20151213

--- lib/openra.rb
+++ lib/openra.rb
@@ -4,7 +4,7 @@ DOWNLOAD_GITHUB_BASE_PATH = "https://github.com/OpenRA/OpenRA/"
 ENABLE_GITHUB_API = true
 
 # Github release IDs: obtain from https://api.github.com/repos/OpenRA/OpenRA/releases
-GITHUB_PLAYTEST_ID = '2202945'
+GITHUB_PLAYTEST_ID = '2280129'
 GITHUB_RELEASE_ID = '1838155'
 
 PAGES = {
