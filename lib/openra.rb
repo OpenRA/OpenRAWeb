aca09d31e78228ae74b5a48fd191f3d9ba9b5851
Paul Chote <pchote@users.noreply.github.com>
2019-11-02 15:17:59 +0000

    Add news post for playtest-20191102.

--- lib/openra.rb
+++ lib/openra.rb
@@ -4,7 +4,7 @@ DOWNLOAD_GITHUB_BASE_PATH = "https://github.com/OpenRA/OpenRA/"
 ENABLE_GITHUB_API = true
 
 # Github release IDs: obtain from https://api.github.com/repos/OpenRA/OpenRA/releases
-GITHUB_PLAYTEST_ID = '20864626'
+GITHUB_PLAYTEST_ID = '21168296'
 GITHUB_RELEASE_ID = '16124869'
 
 PAGES = {
